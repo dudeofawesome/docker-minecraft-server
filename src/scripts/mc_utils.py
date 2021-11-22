1b11a575aca58680458c906679471752d3dba88a
Louis Orleans <louis@orleans.io>
2021-11-22 04:12:01 -0800

    ♻️✨ improve argument handling

--- src/scripts/mc_utils.py
+++ src/scripts/mc_utils.py
@@ -25,3 +25,49 @@ def get_mc_manifest_version(minecraft_version):
     raise "No matching version found"
 
   return minecraft_version, minecraft_major_minor_version, json_url
+
+def get_java_args(RAM_MAX):
+  RAM_MAX = RAM_MAX.upper()
+  max_ram = 0
+  if RAM_MAX.endswith('G'):
+    max_ram = double(RAM_MAX.replace('G', ''))
+  elif RAM_MAX.endswith('M'):
+    max_ram = double(RAM_MAX.replace('M', '')) / 1024
+
+  G1NewSizePercent = 30
+  G1MaxNewSizePercent = 40
+  G1HeapRegionSize = 8
+  G1ReservePercent = 20
+  InitiatingHeapOccupancyPercent = 15
+
+  if max_ram >= 12:
+    G1NewSizePercent = 40
+    G1MaxNewSizePercent = 50
+    G1HeapRegionSize = 16
+    G1ReservePercent = 15
+    InitiatingHeapOccupancyPercent = 20
+
+  return ' '.join([
+    f'-Xms{max_ram}G',
+    f'-Xmx{max_ram}G',
+    f'-XX:+UseG1GC',
+    f'-XX:+ParallelRefProcEnabled',
+    f'-XX:MaxGCPauseMillis=200',
+    f'-XX:+UnlockExperimentalVMOptions',
+    f'-XX:+DisableExplicitGC',
+    f'-XX:+AlwaysPreTouch',
+    f'-XX:G1NewSizePercent={G1NewSizePercent}',
+    f'-XX:G1MaxNewSizePercent={G1MaxNewSizePercent}',
+    f'-XX:G1HeapRegionSize={G1HeapRegionSize}M',
+    f'-XX:G1ReservePercent={G1ReservePercent}',
+    f'-XX:G1HeapWastePercent=5',
+    f'-XX:G1MixedGCCountTarget=4',
+    f'-XX:InitiatingHeapOccupancyPercent={InitiatingHeapOccupancyPercent}',
+    f'-XX:G1MixedGCLiveThresholdPercent=90',
+    f'-XX:G1RSetUpdatingPauseTimePercent=5',
+    f'-XX:SurvivorRatio=32',
+    f'-XX:+PerfDisableSharedMem',
+    f'-XX:MaxTenuringThreshold=1',
+    f'-Dusing.aikars.flags=https://mcflags.emc.gs',
+    f'-Daikars.new.flags=true',
+  ])
